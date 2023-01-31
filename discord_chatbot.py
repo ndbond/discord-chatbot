6283b150acd0301a6f240c30c1138538ffe69a18
ndbond <37131402+ndbond@users.noreply.github.com>
2023-01-30 16:12:36 -0800

    chatgpt integration

--- discord_chatbot.py
+++ discord_chatbot.py
@@ -1,19 +1,45 @@
 import discord
+from revChatGPT.ChatGPT import Chatbot
+import threading
 
 # retrieve authentication token from token.txt
-f = open("token.txt", "r");
-token = f.read();
+f = open("discord.txt", "r");
+discordToken = f.read();
+f.close();
+
+f = open("chatgpt.txt", "r");
+gptToken = f.read();
+f.close();
+
+
+chatbot = Chatbot({
+  "session_token": gptToken
+}, conversation_id=None, parent_id=None)
 
 class MyClient(discord.Client):
+    msgBuffer = ""
+
+    async def tryToRespond(self, message):
+    # only respond to other people whom post in chatbot channel
+        try:
+            response = chatbot.ask(MyClient.msgBuffer)
+        except:
+            print("Rate limit hit. Retry later.")
+        else:
+            MyClient.msgBuffer = ""
+            await message.reply(response['message'], mention_author=False)
+                
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
         print(f'{message.channel} by {message.author}')
-
-        # only respond to other people whom post in chatbot channel
         if (message.author != self.user and message.channel.name == "chatbot"):
-            await message.reply("hi there!", mention_author=False);
+            MyClient.msgBuffer = MyClient.msgBuffer + message.content
+            await self.tryToRespond(message)
+        
+        
+
 
 # set intents
 intents = discord.Intents.default()
@@ -22,4 +48,4 @@ intents.message_content = True
 
 # run client
 client = MyClient(intents=intents)
-client.run(token)
+client.run(discordToken)
